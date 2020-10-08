a123fb0850346f08aa296b744e6939cd2423cbf3
Gabriel Bergoc <59211540+gabrielcb17@users.noreply.github.com>
2020-10-08 15:13:58 -0300

    Adicionado argumento opcional de coringas
    default: joker=0

--- baralho.py
+++ baralho.py
@@ -1,9 +1,11 @@
 class Baralho():
 
-    def __init__(self):
+    def __init__(self, joker=0):
         self.baralho = []
         self.naipes = ['Paus', 'Ouros', 'Copas', 'Espadas']
         for i in range(4):
             for j in range(13):
                 carta = [(j+1), self.naipes[i]]
                 self.baralho.append(carta)
+        for i in range(joker):
+            self.baralho.append(['Coringa'])
